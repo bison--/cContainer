b14edfecd061bbc57111567a00b4daa444cb3f6d
bison-- <mike.bison42@googlemail.com>
2022-05-02 08:04:59 +0200

    works, kinda

---
+++ code/hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+# c99
+void printMessage();
+
+int main(int argc, char const *argv[])
+{
+    printMessage();
+    return 0;
+}
+
+void printMessage() {
+    printf("HELLO WORLD\n");
+}
---
+++ run.bat
@@ -0,0 +1,1 @@
+docker run --rm -it -v %cd%:/coding ccoding ./hello.bin
